32a0dea5ca85fad739730f2192a7a1b44f734d15
facundo ramirez <ramirezfacundo1996@gmail.com>
2024-02-21 03:33:28 -0300

    nuevaVersion

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,29 +1,68 @@
-const fs = require('fs');
-const path = require('path');
+const db = require("../database/models");
+const { Op } = require("sequelize");
+const fs = require("fs");
+const path = require("path");
 
-const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
-const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
-
-const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 const controller = {
-	index: (req, res) => {
-		// Do the magic
-		return res.render('index',{
-			productsVisited : products.filter(product => product.category === "visited"),
-			products,
-			toThousand
-		})
-	},
-	search: (req, res) => {
-		// Do the magic
-		const {keywords	} =req.query;
-		return res.render('results',{
-			products : products.filter(product => product.name.toLowerCase().includes(keywords.toLowerCase()) ),
-			keywords,
-			toThousand
-		})
-	},
+  index: (req, res) => {
+    // Do the magic
+
+    /* db.Product.findAll()
+      .then((products) => {
+        return res.render("index", {
+          productsVisited: products.filter(
+            (product) => product.categoryId === 1
+          ),
+          products,
+          toThousand,
+        });
+      })
+      .catch((error) => console.log(error)); */
+
+    const productsVisited = db.Product.findAll({
+      where: {
+        categoryId: 1,
+      },
+    });
+
+    const productsInSale = db.Product.findAll({
+      where: {
+        categoryId: 2,
+      },
+    });
+
+    Promise.all([productsVisited, productsInSale])
+      .then(([productsVisited, productsInSale]) => {
+        return res.render("index", {
+          productsVisited,
+          productsInSale,
+          toThousand,
+        });
+      })
+      .catch((error) => console.log(error));
+  },
+  search: (req, res) => {
+    // Do the magic
+    const { keywords } = req.query;
+
+    db.Product.findAll({
+      where: {
+        name: {
+          [Op.substring]: keywords,
+        },
+      },
+    })
+      .then((products) => {
+        return res.render("results", {
+          products,
+          keywords,
+          toThousand,
+        });
+      })
+      .catch((error) => console.log(error));
+  },
 };
 
 module.exports = controller;
--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,108 +1,131 @@
-const fs = require('fs');
-const path = require('path');
+const { response } = require("express");
+const db = require("../database/models");
 
-const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
-const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+const fs = require("fs");
+const path = require("path");
 
-const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+const productsFilePath = path.join(__dirname, "../data/productsDataBase.json");
+const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
+
+const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 const controller = {
-	// Root - Show all products
-	index: (req, res) => {
-		// Do the magic
-		return res.render('products',{
-			products,
-			toThousand,
-		})
-	},
+  // Root - Show all products
+  index: (req, res) => {
+    // Do the magic
+    db.Product.findAll()
+      .then((products) => {
+        return res.render("products", {
+          products,
+          toThousand,
+        });
+      })
+      .catch((error) => console.log(error));
+  },
 
-	// Detail - Detail from one product
-	detail: (req, res) => {
-		// Do the magic
-		const product = products.find(product => product.id === +req.params.id)
-		return res.render('detail',{
-			...product,
-			toThousand,
-		})
-	},
+  // Detail - Detail from one product
+  detail: (req, res) => {
+    // Do the magic
 
+    db.Product.findByPk(req.params.id)
+      .then((product) => {
+        return res.render("detail", {
+          ...product.dataValues,
+          toThousand,
+        });
+      })
+      .catch((error) => console.log(error));
+  },
 
-	// Create - Form to create
-	create: (req, res) => {
+  // Create - Form to create
+  create: (req, res) => {
+    // Do the magic
+    db.Category.findAll({
+      order: ["name"],
+    })
+      .then((categories) => {
+        return res.render("product-create-form", {
+          categories,
+        });
+      })
+      .catch((error) => console.log(error));
+  },
 
+  // Create -  Method to store
+  store: (req, res) => {
+    const { name, description, price, discount, categoryId } = req.body;
+    db.Product.create({
+      name: name.trim(),
+      price,
+      discount,
+      description: description.trim(),
+      categoryId,
+    })
+      .then((newProduct) => {
+        return res.redirect("/products/detail/" + newProduct.id);
+      })
+      .catch((error) => console.log(error));
+  },
 
-		// Do the magic
-		return res.render('product-create-form')
-	},
-	
-	// Create -  Method to store
-	store: (req, res) => {
-		const lastID =products[products.length -1].id;
-		const {name, price, discount, category,description} = req.body;
+  // Update - Form to edit
+  edit: (req, res) => {
+    // Do the magic
+    const categories = db.Category.findAll({
+      order: ["name"],
+    });
 
-		const newProduct = {
-			id: lastID +1,
-			name: name.trim(),
-			price: +price,
-			discount: +discount,
-			category: category,
-			description: description.trim(),
-			image: "default-image.png",
-		};
+    const product = db.Product.findByPk(req.params.id);
 
-		products.push(newProduct)
-		fs.writeFileSync(productsFilePath, JSON.stringify(products), 'utf-8')
-		
-		return res.redirect("/products/detail/" +newProduct.id);
-	 
-},
+    Promise.all([categories, product])
+      .then(([categories, product]) => {
+        return res.render("product-edit-form", {
+          ...product.dataValues,
+          categories,
+        });
+      })
+      .catch((error) => console.log(error));
+  },
+  // Update - Method to update
+  update: (req, res) => {
+    // Do the magic
 
+    const { name, price, discount, categoryId, description } = req.body;
 
-	// Update - Form to edit
-	edit: (req, res) => {
-		// Do the magic
-		const product = products.find(product => product.id === +req.params.id);
-		return res.render('product-edit-form',{
-			...product
-		})
-	},
-	// Update - Method to update
-	update: (req, res) => {
-		// Do the magic
-		
-		const {name, price, discount, category,description} = req.body;
-		const productsUpdated = products.map(product => {
-			if( product.id === +req.params.id){
-				
-				product.name = name.trim()
-				product.price = +price
-				product.discount = +discount
-				product.category = category
-				product.description = description.trim()
-			}
-			return product
-		})
-		
+    db.Product.update(
+      {
+        name: name.trim(),
+        price,
+        discount,
+        description: description.trim(),
+        categoryId,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+        .then((response) => {
+          console.log(response);
+          return res.redirect("/products/detail/" + req.params.id);
+        })
+        .catch((error) => console.log(error))
+    );
+  },
 
-	
-		fs.writeFileSync(productsFilePath, JSON.stringify(productsUpdated), 'utf-8')
-		
-		return res.redirect("/products/detail/"+ req.params.id);
-	},              
-
-	// Delete - Delete one product from DB
-	destroy : (req, res) => {
-		// Do the magic
-		
-			const productId = +req.params.id;
-			const productIndex = products.findIndex(product => product.id === productId);
-			if (productIndex !== -1) {
-			  products.splice(productIndex, 1); 
-			  fs.writeFileSync(productsFilePath, JSON.stringify(products), 'utf-8');
-			 return  res.redirect('/products');
-			}
-		
-		  }
-	};
+  // Delete - Delete one product from DB
+  destroy: (req, res) => {
+    // Do the magic
+    db.Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    })
+      .then((response) => {
+        console.log(response);
+        return res.redirect("/products");
+      })
+      .catch((error) => console.log(error));
+  },
+};
 
 module.exports = controller;
